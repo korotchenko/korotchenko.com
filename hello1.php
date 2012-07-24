773da0f0c780b6a156779e4d74172fd1e251ae16
tanya <tetiana.korotchenko@gmail.com>
2012-07-24 16:44:30 +0300

    [-] rename

---
+++ hello1.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+      <title>Test</title>
+    </head>
+    <body>
+      <?php echo '<p>Hello World</p>'; ?>
+    </body>
+  </html>
+<form name="input" action="" method="get">
+    <label for="user">
+        Имя пользователя:
+        <input type="text" name="user" id="user"/>
+    </label>
+    <input type="submit" value="Submit" />
+</form>
+<html>
+<head>
+    <script type="text/javascript" src="">
+    </script>
+    <script>
+        jQuery(document).ready(function()
+            {
+                alert('Hello World');
+            }
+        );
+    </script>
+</head>
+<body>
+<h1>jQuery Tutorials</h1>
+</body>
+</html>
